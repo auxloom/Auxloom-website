1decefbaaa0312a0e1a2f44b63f67c684215e409
AjXKai <abhishek730017@gmail.com>
2026-07-03 11:13:47 +0530

    fix: remove lightningcss and babel-plugin-react-compiler to fix cross-platform build

--- vite.config.ts
+++ vite.config.ts
@@ -31,9 +31,6 @@ export default defineConfig(async ({ command }) => {
   }
 
   return {
-    css: {
-      transformer: "lightningcss",
-    },
     resolve: {
       alias: {
         "@": path.resolve(process.cwd(), "./src"),
